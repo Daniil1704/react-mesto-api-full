a1e482cd56def70c687d7168ef7f556a3d83bbb8
Daniil Tsyrulnikov <tyron1996sweg@gmail.com>
2020-10-28 16:02:11 +0300

    Проектная работа 15

--- routes/cards.js
+++ routes/cards.js
@@ -16,25 +16,25 @@ router.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().custom(validatorLink),
-  }).unknown(true),
+  })
 }), buildCard);
 
 router.delete('/cards/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().required().hex().length(24),
-  }).unknown(true),
+  })
 }), deleteCard);
 
 router.put('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required().hex(),
-  }).unknown(true),
+    cardId: Joi.string().required().hex().length(24),
+  })
 }), like);
 
 router.delete('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required().hex(),
-  }).unknown(true),
+    cardId: Joi.string().required().hex().length(24),
+  })
 }), deleteLike);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,7 @@ router.get('/users', getUsers);
 router.get('/users/me', celebrate({
   headers: Joi.object().keys({
     authorization: Joi.string().required(),
-  }).unknown(true),
+  })
 }), getUser);
 
 router.get('/users/:userId', celebrate({
@@ -28,13 +28,13 @@ router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
-  }).unknown(true),
+  })
 }), changeUser);
 
 router.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().custom(validatorLink),
-  }).unknown(true),
+  })
 }), changeAvatar);
 
 module.exports = router;
